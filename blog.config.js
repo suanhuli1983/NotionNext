c1759373ef3250b32c907a461e3ffc7c99e0b2ef
suanhuli1983 <112803628+suanhuli1983@users.noreply.github.com>
2025-06-30 11:36:17 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -11,7 +11,7 @@ const BLOG = {
   APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'auto', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '酸狐狸', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个寂寞的人🍚', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '一亩三分地🍚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://suanhuli.zhang.icu', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
